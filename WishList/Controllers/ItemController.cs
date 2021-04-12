a651935d3c3f61aaf8ebaaaad87c6ae6bd6c8332
nmmufti <80278616+nmmufti@users.noreply.github.com>
2021-04-12 15:41:52 +0100

    Step6_1

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -11,6 +11,11 @@ namespace WishList.Controllers
     public class ItemController : Controller
     {
         private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View("Index",_context.Items);
